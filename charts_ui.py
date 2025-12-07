0ae176e20265d284946b8a35ed10314db50d5652
tkg-create <timmokg@gmail.com>
2025-12-07 17:23:17 -0500

    TKG Contributions

--- charts_ui.py
+++ charts_ui.py
@@ -1,7 +1,6 @@
 from tkinter import ttk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from calendar_ui import get_selected_year_month
 from data_fetch import data_fetcher
 
 
